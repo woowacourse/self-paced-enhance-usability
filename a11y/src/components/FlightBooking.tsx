2a2e6d67a0d8a2967fa895aee5a0d448f0f05df3
손진영 <sm099@naver.com>
2024-09-16 20:39:08 +0900

    feat: 시멘틱 태그 적용

--- a11y/src/components/FlightBooking.tsx
+++ a11y/src/components/FlightBooking.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import "./FlightBooking.css";
+import './FlightBooking.css';
 
 const MAX_PASSENGERS = 3;
 
@@ -16,7 +16,7 @@ const FlightBooking = () => {
   };
 
   return (
-    <div className="flight-booking">
+    <section className="flight-booking">
       <h2 className="heading-2-text">항공권 예매</h2>
       <div className="passenger-count">
         <span className="body-text">성인</span>
@@ -31,7 +31,7 @@ const FlightBooking = () => {
         </div>
       </div>
       <button className="search-button">항공편 검색</button>
-    </div>
+    </section>
   );
 };
 
