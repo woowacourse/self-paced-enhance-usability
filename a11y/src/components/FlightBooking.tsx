e9a73d812b542f24bf571b650afee7c79aac9d49
useon <roqkftjs@gmail.com>
2024-09-16 21:33:22 +0900

    feat: 승객 감소/추가 버튼에 의미 있는 레이블 설정

--- a11y/src/components/FlightBooking.tsx
+++ a11y/src/components/FlightBooking.tsx
@@ -21,16 +21,26 @@ const FlightBooking = () => {
       <div className='passenger-count'>
         <span className='body-text'>성인</span>
         <div className='counter'>
-          <button className='button-text' onClick={decrementCount}>
+          <button
+            aria-label='성인 승객 감소'
+            className='button-text'
+            onClick={decrementCount}
+          >
             -
           </button>
           <span>{adultCount}</span>
-          <button className='button-text' onClick={incrementCount}>
+          <button
+            aria-label='성인 승객 증가'
+            className='button-text'
+            onClick={incrementCount}
+          >
             +
           </button>
         </div>
       </div>
-      <button className='search-button'>항공편 검색</button>
+      <button role='button' className='search-button'>
+        항공편 검색
+      </button>
     </section>
   );
 };
