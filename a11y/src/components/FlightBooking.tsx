e2756286023f7202ac365ff4c666fb5da4fe81bd
hanyoung <phanyoung@naver.com>
2024-09-17 15:53:15 +0900

    feat: 최소/최대 값 도달 시 상태 메시지 알림 추가하기

--- a11y/src/components/FlightBooking.tsx
+++ a11y/src/components/FlightBooking.tsx
@@ -1,37 +1,46 @@
+import { useState } from "react";
+
 import "./FlightBooking.css";
-import { useCounter } from "../hooks/useCounter";
 
 const MIN_PASSENGERS = 1;
 const MAX_PASSENGERS = 3;
 
-const ALERT_MESSAGE = {
-  min: "최소 승객 수에 도달했습니다.",
-  max: "최대 승객 수에 도달했습니다.",
-};
-
 const FlightBooking = () => {
-  const {
-    count: adultCount,
-    alertMessage,
-    decrementCount,
-    incrementCount,
-  } = useCounter({
-    min: {
-      value: MIN_PASSENGERS,
-      alertMessage: ALERT_MESSAGE.min,
-    },
-    max: {
-      value: MAX_PASSENGERS,
-      alertMessage: ALERT_MESSAGE.max,
-    },
-  });
+  const [adultCount, setAdultCount] = useState(1);
+  const [statusMessage, setStatusMessage] = useState("");
+
+  const resetStatusMessage = () => {
+    setTimeout(() => {
+      setStatusMessage("");
+    });
+  };
+
+  const incrementCount = () => {
+    if (adultCount === MAX_PASSENGERS) {
+      setStatusMessage("최대 승객 수에 도달했습니다");
+      resetStatusMessage();
+      return;
+    }
+
+    setAdultCount((prev) => Math.min(MAX_PASSENGERS, prev + 1));
+  };
+
+  const decrementCount = () => {
+    if (adultCount === MIN_PASSENGERS) {
+      setStatusMessage("최소 1명의 승객이 필요합니다");
+      resetStatusMessage();
+      return;
+    }
+
+    setAdultCount((prev) => Math.max(1, prev - 1));
+  };
 
   return (
-    <article className="flight-booking">
-      <h1 className="heading-2-text">항공권 예매</h1>
+    <div className="flight-booking">
+      <h2 className="heading-2-text">항공권 예매</h2>
       <div className="passenger-count">
         <span className="body-text">성인</span>
-        <section className="counter">
+        <div className="counter">
           <button className="button-text" onClick={decrementCount} aria-label="성인 승객 감소">
             -
           </button>
@@ -39,15 +48,15 @@ const FlightBooking = () => {
           <button className="button-text" onClick={incrementCount} aria-label="성인 승객 증가">
             +
           </button>
-        </section>
-        {alertMessage && (
-          <span className="visually-hidden" role="alert">
-            {alertMessage}
-          </span>
-        )}
+        </div>
       </div>
+      {statusMessage && (
+        <div className="visually-hidden" role="alert">
+          {statusMessage}
+        </div>
+      )}
       <button className="search-button">항공편 검색</button>
-    </article>
+    </div>
   );
 };
 
