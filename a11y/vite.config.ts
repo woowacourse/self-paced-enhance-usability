b4919fc4bfbe5e2023553708c30daf64b1f837d2
ooherin <555ohr@naver.com>
2024-09-22 02:44:27 +0900

    feat: 언어 변경

--- a11y/vite.config.ts
+++ a11y/vite.config.ts
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // 네트워크 상의 다른 장치에서도 접근할 수 있게 함
+    port: 3000, // 기본적으로 3000 포트를 사용
+  },
 });
