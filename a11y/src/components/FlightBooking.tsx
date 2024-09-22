138c238a36d5f9899f770bbd67666300f02c5de9
ooherin <555ohr@naver.com>
2024-09-22 10:04:30 +0900

    feat: 인원수 제한 메세지 생성

--- a11y/src/components/FlightBooking.tsx
+++ a11y/src/components/FlightBooking.tsx
@@ -3,15 +3,27 @@ import { useState } from "react";
 import "./FlightBooking.css";
 
 const MAX_PASSENGERS = 3;
+const MIN_PASSENGERS = 1;
 
 const FlightBooking = () => {
   const [adultCount, setAdultCount] = useState(1);
+  const [countStatusMessage, setCountStatusMessage] = useState("");
 
   const incrementCount = () => {
+    if (adultCount === MAX_PASSENGERS) {
+      setCountStatusMessage("최대 승객 수에 도달했습니다.");
+      return;
+    }
+
     setAdultCount((prev) => Math.min(MAX_PASSENGERS, prev + 1));
   };
 
   const decrementCount = () => {
+    if (adultCount === MIN_PASSENGERS) {
+      setCountStatusMessage("최소 1명의 승객이 필요합니다.");
+      return;
+    }
+
     setAdultCount((prev) => Math.max(1, prev - 1));
   };
 
@@ -21,13 +33,24 @@ const FlightBooking = () => {
       <div className="passenger-count">
         <span className="body-text">성인</span>
         <div className="counter">
-          <button className="button-text" onClick={decrementCount}>
+          <button
+            className="button-text"
+            onClick={decrementCount}
+            aria-label={"성인 승객 감소"}
+          >
             -
           </button>
-          <span>{adultCount}</span>
-          <button className="button-text" onClick={incrementCount}>
+          <span aria-live="polite">{adultCount}</span>
+          <button
+            className="button-text"
+            onClick={incrementCount}
+            aria-label={"성인 승객 증가"}
+          >
             +
           </button>
+        </div>
+        <div className="visullay-hidden" role="alert">
+          {countStatusMessage}
         </div>
       </div>
       <button className="search-button">항공편 검색</button>
