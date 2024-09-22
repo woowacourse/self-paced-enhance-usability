9860c13f05662e7640e9b35bbaf981d6da3af840
river <130737187+0jenn0@users.noreply.github.com>
2024-09-22 19:49:40 +0900

    feat(FlightBooking): 최소/최대 값 도달 시 상태 메시지 알림 추가

--- a11y/src/components/FlightBooking/FlightBooking.tsx
+++ a11y/src/components/FlightBooking/FlightBooking.tsx
@@ -1,26 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./FlightBooking.css";
+import Toast from "../Toast/Toast";
 
 const MAX_PASSENGERS = 3;
+const MIN_PASSENGERS = 1;
+
+const STATUS_MESSAGE = {
+  increase: "성인 승객 증가 ",
+  decrease: "성인 승객 감소 ",
+};
+
+const ALERT_MESSAGE = {
+  min: "최소 승객 수에 도달했습니다.",
+  max: "최대 승객 수에 도달했습니다.",
+};
 
 const FlightBooking = () => {
-  const [adultCount, setAdultCount] = useState(1);
+  const [adultCount, setAdultCount] = useState(MIN_PASSENGERS);
   const [statusMessage, setStatusMessage] = useState<string | null>(null);
+  const [toastMessage, setToastMessage] = useState<string | null>(null);
 
-  const STATUS_MESSAGE = {
-    increase: "성인 승객 증가 ",
-    decrease: "성인 승객 감소 ",
-  };
+  useEffect(() => {
+    if (toastMessage) {
+      const timer = setTimeout(() => {
+        setToastMessage(null);
+      }, 1000 * 3);
+
+      return () => clearTimeout(timer);
+    }
+  }, [toastMessage]);
 
   const incrementCount = () => {
     setStatusMessage(STATUS_MESSAGE.increase);
     setAdultCount((prev) => Math.min(MAX_PASSENGERS, prev + 1));
+
+    if (adultCount === MAX_PASSENGERS) {
+      setToastMessage(ALERT_MESSAGE.max);
+    }
   };
 
   const decrementCount = () => {
     setStatusMessage(STATUS_MESSAGE.decrease);
-    setAdultCount((prev) => Math.max(1, prev - 1));
+    setAdultCount((prev) => Math.max(MIN_PASSENGERS, prev - 1));
+
+    if (adultCount === MIN_PASSENGERS) {
+      setToastMessage(ALERT_MESSAGE.min);
+    }
   };
 
   return (
@@ -50,6 +76,7 @@ const FlightBooking = () => {
         </div>
       </div>
       <button className="search-button">항공편 검색</button>
+      {toastMessage && <Toast>{toastMessage}</Toast>}
     </div>
   );
 };
--- a11y/src/components/index.ts
+++ a11y/src/components/index.ts
@@ -1,1 +1,2 @@
+export { default as FlightBooking } from "./FlightBooking/FlightBooking";
 export { default as Toast } from "./Toast/Toast";
