39455c57e408e906d0f627a891ee74fe1366b868
Lee sang Yeop <mma7710@naver.com>
2024-09-23 22:10:37 +0900

    WAI-ARIA 추가

--- a11y/src/main.tsx
+++ a11y/src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ToastContainer
+      autoClose={1000}
+      pauseOnFocusLoss={false}
+      pauseOnHover={false}
+      limit={1}
+    />
     <App />
   </StrictMode>
 );
