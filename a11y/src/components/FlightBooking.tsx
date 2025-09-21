b9920564f9a46e5858857dd527a334d0f5037cd2
Jeongeun Lee <leeju1009@gmail.com>
2025-09-21 20:38:37 +0900

    feat: 한계 승객 수 도달 시, 메시지를 표시하는 기능 추가

--- a11y/src/components/FlightBooking.tsx
+++ a11y/src/components/FlightBooking.tsx
@@ -2,17 +2,41 @@ import { useState } from 'react';
 
 import './FlightBooking.css';
 
-const MAX_PASSENGERS = 3;
+const MIN_COUNT = 1;
+const MAX_COUNT = 3;
 
 const FlightBooking = () => {
   const [adultCount, setAdultCount] = useState(1);
+  const [adultCountMessage, setAdultCountMessage] = useState('');
+
+  const getLimitCountMessage = (count: number, prevCount: number) => {
+    if (count === MAX_COUNT && prevCount === MAX_COUNT) {
+      return '최대 승객 수에 도달했습니다.';
+    }
+
+    if (count === MIN_COUNT && prevCount === MIN_COUNT) {
+      return '최소 승객 수에 도달했습니다.';
+    }
+
+    return '';
+  };
 
   const incrementCount = () => {
-    setAdultCount((prev) => Math.min(MAX_PASSENGERS, prev + 1));
+    setAdultCount((prev) => {
+      const next = Math.min(MAX_COUNT, prev + 1);
+      const message = getLimitCountMessage(next, prev);
+      setAdultCountMessage(message);
+      return next;
+    });
   };
 
   const decrementCount = () => {
-    setAdultCount((prev) => Math.max(1, prev - 1));
+    setAdultCount((prev) => {
+      const next = Math.max(MIN_COUNT, prev - 1);
+      const message = getLimitCountMessage(next, prev);
+      setAdultCountMessage(message);
+      return next;
+    });
   };
 
   return (
@@ -20,6 +44,14 @@ const FlightBooking = () => {
       <h2 className='heading-2-text'>항공권 예매</h2>
       <div className='passenger-count'>
         <span className='body-text'>성인</span>
+        <p
+          id='adult-count-message'
+          className='adultCount-notice'
+          aria-live='polite'
+          role='status'
+        >
+          {adultCountMessage}
+        </p>
         <div className='counter'>
           <button
             className='button-text'
