66badd3481d4417e2088781da9f38ff7c342b521
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-09-29 21:36:06 +0900

    style: FlightBooking.tsx에서 세미콜론 제거 및 코드 포맷 정리

--- a11y/src/components/FlightBooking.tsx
+++ a11y/src/components/FlightBooking.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import "./FlightBooking.css";
+import "./FlightBooking.css"
 
-const MAX_PASSENGERS = 3;
+const MAX_PASSENGERS = 3
 
 const FlightBooking = () => {
-  const [adultCount, setAdultCount] = useState(1);
+  const [adultCount, setAdultCount] = useState(1)
 
   const incrementCount = () => {
-    setAdultCount((prev) => Math.min(MAX_PASSENGERS, prev + 1));
-  };
+    setAdultCount((prev) => Math.min(MAX_PASSENGERS, prev + 1))
+  }
 
   const decrementCount = () => {
-    setAdultCount((prev) => Math.max(1, prev - 1));
-  };
+    setAdultCount((prev) => Math.max(1, prev - 1))
+  }
 
   return (
     <div className="flight-booking">
@@ -21,18 +21,24 @@ const FlightBooking = () => {
       <div className="passenger-count">
         <span className="body-text">성인</span>
         <div className="counter">
-          <button className="button-text" onClick={decrementCount}>
+          <button
+            className="button-text"
+            onClick={decrementCount}
+          >
             -
           </button>
           <span>{adultCount}</span>
-          <button className="button-text" onClick={incrementCount}>
+          <button
+            className="button-text"
+            onClick={incrementCount}
+          >
             +
           </button>
         </div>
       </div>
       <button className="search-button">항공편 검색</button>
     </div>
-  );
-};
+  )
+}
 
-export default FlightBooking;
+export default FlightBooking
